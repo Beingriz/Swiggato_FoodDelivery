4d184d5e59c7d990b436539a10bf5c3f5252cccf
Md Rizwan <68804148+Beingriz@users.noreply.github.com>
2023-10-08 11:50:29 +0530

    Restaurant Update Status API Completed

--- src/main/java/com/example/Swiggato/conroller/RestaurantController.java
+++ src/main/java/com/example/Swiggato/conroller/RestaurantController.java
@@ -6,10 +6,7 @@ import com.example.Swiggato.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/restaurant")
@@ -23,5 +20,15 @@ public class RestaurantController {
     public ResponseEntity addRestaurant(@RequestBody RestaurantRequest restaurantRequest){
         RestaurantResponse restaurantResponse = restaurantService.addRestaurant(restaurantRequest);
         return new ResponseEntity<>(restaurantResponse, HttpStatus.CREATED);
+    }
+    @PutMapping("/update/status")
+    public ResponseEntity openOrCloseRestaurant(@RequestParam int id){
+        try {
+            String response = restaurantService.openOrCloseRestaurant(id);
+            return new ResponseEntity<>(response,HttpStatus.ACCEPTED);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+
     }
 }
--- src/main/java/com/example/Swiggato/service/RestaurantService.java
+++ src/main/java/com/example/Swiggato/service/RestaurantService.java
@@ -2,11 +2,14 @@ package com.example.Swiggato.service;
 
 import com.example.Swiggato.dto.request.RestaurantRequest;
 import com.example.Swiggato.dto.response.RestaurantResponse;
+import com.example.Swiggato.exception.RestaurantNotFoundException;
 import com.example.Swiggato.model.Restaurant;
 import com.example.Swiggato.repisoory.RestaurantRepository;
 import com.example.Swiggato.transformer.RestaurantTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RestaurantService {
@@ -26,5 +29,20 @@ public class RestaurantService {
         // Converto Response DTO
 
         return RestaurantTransformer.restaurantToRestaurantResponse(savedrestaurant);
+    }
+
+    public String openOrCloseRestaurant(int id) {
+      Optional<Restaurant> restaurantOptional = restaurantRepository.findById(id);
+      if(restaurantOptional.isEmpty()){
+          throw new RestaurantNotFoundException("Invalid Restaurant Id!");
+      }
+      Restaurant restaurant = restaurantOptional.get();
+      restaurant.setOpened(!restaurant.isOpened());
+      restaurantRepository.save(restaurant);
+
+      if(restaurant.isOpened()){
+          return " Retaurant is now Opend!..";
+      }
+      return " Restaurant is now Closed";
     }
 }
